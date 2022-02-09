1a20725268036046156b83e09b4972e0d68e96ef
Jr Sarath <jrsarath@outlook.com>
2022-02-09 13:10:50 +0530

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,24 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+$options = array(
+	'wpem_rest_api_version',
+	'wpem_primary_color',
+	'wpem_success_color',
+	'wpem_info_color',
+	'wpem_warning_color',
+	'wpem_danger_color',
+	'wpem_primary_dark_color',
+	'wpem_success_dark_color',
+	'wpem_info_dark_color',
+	'wpem_warning_dark_color',
+	'wpem_danger_dark_color',
+	'wpem_app_branding_settings',
+	'wpem_app_branding_dark_settings',
+);
+
+foreach ( $options as $option ) {
+	delete_option( $option );
+}
