322b996accd475812287398ffbf3c9d6e5f7b2ad
Hiren <sangani.reeta@gmail.com>
2023-09-01 15:39:38 +0530

    Unalbe to change title name #78

--- admin/wpem-rest-api-settings.php
+++ admin/wpem-rest-api-settings.php
@@ -1,108 +1,90 @@
 <?php
-/*
-* This file use for settings at admin site for wp event manager plugin.
-*/
-
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
- * WPEM_Rest_API_Settings class.
+ * WPEM_Rest_API_Settings class used to create settings fields and save settings.
  */
-
 class WPEM_Rest_API_Settings {
 
 	public $settings;
-
 	/**
 	 * __construct function.
-	 *
 	 * @access public
 	 * @return void
 	 */
 
 	public function __construct() {
-
 		$this->settings_group = 'wpem_rest_api';
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
 	/**
 	 * init_settings function.
-	 *
 	 * @access public
 	 * @return void
 	 */
 	public function init_settings() {
-
 		$this->settings = apply_filters( 'wpem_rest_api_settings',
-
 			array(
-					'general' => array(
-							'label'			=>	__( 'General', 'wpem-rest-api' ),
-							'icon'			=>	'meter',
-							'type'       => 'fields',
-							'sections'		=> array(
-									'general' => __('General Settings','wpem-rest-api'),
+				'general' => array(
+					'label'	   => __( 'General', 'wpem-rest-api' ),
+					'icon'	   => 'meter',
+					'type'     => 'fields',
+					'sections' => array(
+						'general' => __('General Settings','wpem-rest-api'),
+					), 
+					'fields' => array(
+						'general' => array(
+							array(
+								'name'       => 'enable_wpem_rest_api',
+								'std'        => '1',
+								'label'      => __( 'Enable Rest API', 'wpem-rest-api' ),
+								'cb_label'   => __( 'Disable to remove the API functionality from your event website.', 'wpem-rest-api' ),
+								'desc'       => '',
+								'type'       => 'checkbox',
+								'attributes' => array(),
 							),
-							'fields'		=> array(
-									'general' => array(
-											array(
-												'name'       => 'enable_wpem_rest_api',
-												'std'        => '1',
-												'label'      => __( 'Enable Rest API', 'wpem-rest-api' ),
-												'cb_label'   => __( 'Disable to remove the API functionality from your event website.', 'wpem-rest-api' ),
-												'desc'       => '',
-												'type'       => 'checkbox',
-												'attributes' => array(),
-											),
-											
-											array(
-												'name'       => 'wpem_rest_api_app_name',
-												'std'        => 'WP Event Manager',
-												'cb_label'   => __( 'Add name of your Application.', 'wpem-rest-api' ),
-												'label'      => __( 'Application name', 'wpem-rest-api' ),
-												'desc'       => __( '', 'wpem-rest-api' ),
-												'type'       => 'text',
-
-												'attributes' => array(),
-											),
-
-											array(
-												'name'       => 'wpem_rest_api_app_logo',
-												'std'        => '',
-												'cb_label'   => __( 'Upload  the logo of your own brand.', 'wpem-rest-api' ),
-												'label'      => __( 'App Logo', 'wpem-rest-api' ),
-												'desc'       => __( 'Upload smallest file possible to ensure lesser loading time', 'wpem-rest-api' ),
-												'type'       => 'file',
-
-												'attributes' => array(),
-											),
-
-									),
-							)
-					),
-					'api-access' => array(
-							'label'			=>	__( 'API Access', 'wpem-rest-api' ),
-							'icon'			=>	'loop',
-							'type'       => 'template',
-					),
-					'app-branding' => array(
-							'label'			=>	__( 'APP Branding', 'wpem-rest-api' ),
-							'icon'			=>	'mobile',
-							'type'       	=> 'template',
-					),
+							array(
+								'name'       => 'wpem_rest_api_app_name',
+								'std'        => 'WP Event Manager',
+								'label'      => __( 'Application Name', 'wpem-rest-api' ),
+								'cb_label'   => __( 'WP Event Manager', 'wpem-rest-api' ),
+								'desc'       => '',
+								'type'       => 'text',
+								'attributes' => array(),
+							),
+							array(
+								'name'       => 'wpem_rest_api_app_logo',
+								'std'        => '',
+								'cb_label'   => __( 'Upload  the logo of your own brand.', 'wpem-rest-api' ),
+								'label'      => __( 'App Logo', 'wpem-rest-api' ),
+								'desc'       => __( 'Upload smallest file possible to ensure lesser loading time', 'wpem-rest-api' ),
+								'type'       => 'file',
+								'attributes' => array(),
+							),
+						),
+					)
+				),
+				'api-access' => array(
+					'label'	=>	__( 'API Access', 'wpem-rest-api' ),
+					'icon'	=>	'loop',
+					'type'  => 'template',
+				),
+				'app-branding' => array(
+					'label'	=>	__( 'APP Branding', 'wpem-rest-api' ),
+					'icon'	=>	'mobile',
+					'type'  => 'template',
+				),
 			)
 		);
 	}
 
 	/**
 	 * register_settings function.
-	 *
 	 * @access public
 	 * @return void
 	 */
 	public function register_settings() {
-
 		$this->init_settings();
 
 		foreach($this->settings as $settings ){
@@ -111,7 +93,6 @@ class WPEM_Rest_API_Settings {
 
 	      		if(isset($settings['fields'][$section_key]))
 		      	foreach ( $settings['fields'][$section_key] as $option ) {
-
 		      		if(isset($option['name']) && isset($option['std']) )
 		      			add_option( $option['name'], $option['std'] );
 
@@ -122,14 +103,11 @@ class WPEM_Rest_API_Settings {
 	}
 
 	/**
-	 * output function.
-	 *
+	 * output function used to display setting fields at backend side in settings.
 	 * @access public
 	 * @return void
 	 */
-
 	public function output() {
-
 		$this->init_settings();
 
 		wp_enqueue_style( 'wpem-rest-api-backend', WPEM_REST_API_PLUGIN_URL.'/assets/css/backend.min.css' );
@@ -140,51 +118,45 @@ class WPEM_Rest_API_Settings {
 		$action = '';
 		if(in_array($current_tab, ['general'])){
 			$action = 'action=options.php';
-		}?>
+		} ?>
 		<div class="wrap">
         	<h1><?php _e( 'Rest API Settings', 'wpem-rest-api' ); ?></h1>
     	</div>
 
 		<div id="wpbody" role="main">
-		  <div id="wpbody-content" class="wpem-admin-container">
+		  	<div id="wpbody-content" class="wpem-admin-container">
+		    	<div class="wpem-wrap">
+					<form method="post" name="wpem-rest-settings-form" <?php echo esc_attr($action); ?> >
 
-		    <div class="wpem-wrap">
-				<form method="post" name="wpem-rest-settings-form" <?php echo esc_attr($action); ?> >
-
-					<?php settings_fields( $this->settings_group ); ?>
-
-					<div class="wpem-admin-left-sidebar">
-				        <ul class="wpem-admin-left-menu">
-
-				        	<?php foreach ( $this->settings as $key => $section ) { ?>
-				          <li class="wpem-admin-left-menu-item">
-				            <a class="wpem-icon-<?php echo isset($section['icon']) ? esc_attr($section['icon']) : 'meter';?> nav-tab <?php if ( isset( $_GET['tab'] ) && ( $_GET['tab'] == $key )  ) echo 'nav-tab-active'; ?>" href="<?php echo  esc_url( admin_url( 'edit.php?post_type=event_listing&page=wpem-rest-api-settings&tab='.$key ) );?>"><?php echo esc_html( $section['label'] ) ;?></a>
-				          </li>
-				       		<?php } ?>
-				        </ul>
-				      </div>
-				      <div class="wpem-admin-right-container wpem-<?php echo esc_html($current_tab); ?> wpem-app-branding-mode wpem-light-mode">
-				      	<div class="metabox-holder wpem-admin-right-container-holder">
-				          <div class="wpem-admin-top-title-section postbox">
-				          	<?php
-                                if (!empty($_GET['settings-updated'])) {
-                                    flush_rewrite_rules();
-                                    echo '<div class="updated fade event-manager-updated"><p>' . __( 'Settings successfully saved', 'wpem-rest-api' ) . '</p></div>';
-                                }	
-				                  include('templates/wpem-rest-settings-panel.php');
-				             ?>
-						  </div>
-						  <p class="submit">
-								<input type="submit" class="button-primary wpem-backend-theme-button" id="save-changes" value="<?php _e( 'Save Changes', 'wpem-rest-api' ); ?>" />
-						  </p>
+						<?php settings_fields( $this->settings_group ); ?>
+						<div class="wpem-admin-left-sidebar">
+							<ul class="wpem-admin-left-menu">
+								<?php foreach ( $this->settings as $key => $section ) { ?>
+									<li class="wpem-admin-left-menu-item">
+										<a class="wpem-icon-<?php echo isset($section['icon']) ? esc_attr($section['icon']) : 'meter';?> nav-tab <?php if ( isset( $_GET['tab'] ) && ( $_GET['tab'] == $key )  ) echo 'nav-tab-active'; ?>" href="<?php echo  esc_url( admin_url( 'edit.php?post_type=event_listing&page=wpem-rest-api-settings&tab='.$key ) );?>"><?php echo esc_html( $section['label'] ) ;?></a>
+									</li>
+								<?php } ?>
+							</ul>
 						</div>
-
-				      </div>
-
-			    </form>
-		    </div>
-		 </div>
+						<div class="wpem-admin-right-container wpem-<?php echo esc_html($current_tab); ?> wpem-app-branding-mode wpem-light-mode">
+							<div class="metabox-holder wpem-admin-right-container-holder">
+								<div class="wpem-admin-top-title-section postbox">
+									<?php
+										if (!empty($_GET['settings-updated'])) {
+											flush_rewrite_rules();
+											echo '<div class="updated fade event-manager-updated"><p>' . __( 'Settings successfully saved', 'wpem-rest-api' ) . '</p></div>';
+										}	
+										include('templates/wpem-rest-settings-panel.php'); ?>
+								</div>
+								<p class="submit">
+										<input type="submit" class="button-primary wpem-backend-theme-button" id="save-changes" value="<?php _e( 'Save Changes', 'wpem-rest-api' ); ?>" />
+								</p>
+							</div>
+						</div>
+			    	</form>
+		    	</div>
+		 	</div>
 		</div>
-		<?php  wp_enqueue_script( 'wp-event-manager-admin-settings');
+		<?php wp_enqueue_script( 'wp-event-manager-admin-settings');
 	}
 }
--- includes/rest-api/wpem-rest-app-branding.php
+++ includes/rest-api/wpem-rest-app-branding.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * REST API Events controller
- *
- * Handles requests to the /events endpoint.
- *
- * @since 1.0.0
- */
-
 defined('ABSPATH') || exit;
 
 /**
@@ -14,25 +6,21 @@ defined('ABSPATH') || exit;
  *
  * @extends WPEM_REST_CRUD_Controller
  */
-class WPEM_REST_APP_Branding_Controller extends WPEM_REST_CRUD_Controller
-{
+class WPEM_REST_APP_Branding_Controller extends WPEM_REST_CRUD_Controller {
     /**
      * Endpoint namespace.
-     *
      * @var string
      */
     protected $namespace = 'wpem';
 
     /**
      * Route base.
-     *
      * @var string
      */
     protected $rest_base = 'branding';
 
     /**
      * Post type.
-     *
      * @var string
      */
     protected $post_type = 'event_listing';
@@ -40,12 +28,10 @@ class WPEM_REST_APP_Branding_Controller extends WPEM_REST_CRUD_Controller
     /**
      * Initialize event actions.
      */
-    public function __construct()
-    {
-        //add_action( "wpem_rest_insert_{$this->post_type}_object", array( $this, 'clear_transients' ) );
+    public function __construct() {
         add_action('rest_api_init', array( $this, 'register_routes' ), 10);
 
-        if(!class_exists('WPEM_Rest_API_Settings')) {
+        if( !class_exists( 'WPEM_Rest_API_Settings' ) ) {
             include_once WPEM_REST_API_PLUGIN_DIR.'/admin/wpem-rest-api-settings.php';
         }
     }
@@ -53,19 +39,16 @@ class WPEM_REST_APP_Branding_Controller extends WPEM_REST_CRUD_Controller
     /**
      * Register the routes for events.
      */
-    public function register_routes()
-    {
+    public function register_routes() {
         register_rest_route(
             $this->namespace,
             '/' . $this->rest_base,
             array(
-            array(
-            'methods'             => WP_REST_Server::READABLE,
-            'callback'            => array( $this, 'get_branding_settings' ),
-            'permission_callback' => array( $this, 'get_items_permissions_check' ),
-            //'args'                => $this->get_collection_params(),
-            )
-            //'schema' => array( $this, 'get_public_item_schema' ),
+                array(
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => array( $this, 'get_branding_settings' ),
+                    'permission_callback' => array( $this, 'get_items_permissions_check' ),
+                )
             )
         );
     }
@@ -76,20 +59,16 @@ class WPEM_REST_APP_Branding_Controller extends WPEM_REST_CRUD_Controller
      * @since 1.0.0
      * @param 
      */
-    public function get_branding_settings()
-    {
+    public function get_branding_settings() {
         $wpem_app_branding_settings = [];
 
-        $wpem_app_branding_settings['app_name'] = get_option('wpem_rest_api_app_name') ? update_option('WP Event Manager', 'wpem_rest_api_app_name') : '';
-        $wpem_app_branding_settings['app_logo'] = get_option('wpem_rest_api_app_logo');
-        $wpem_app_branding_settings['app_splash_screen_image'] = get_option('wpem_rest_api_app_splash_screen_image');
-        $wpem_app_branding_settings['color_scheme'] = get_option('wpem_app_branding_settings');
-        $wpem_app_branding_settings['dark_color_scheme'] = get_option('wpem_app_branding_dark_settings');
+        $wpem_app_branding_settings['app_name'] = get_option( 'wpem_rest_api_app_name' );
+        $wpem_app_branding_settings['app_logo'] = get_option( 'wpem_rest_api_app_logo' );
+        $wpem_app_branding_settings['app_splash_screen_image'] = get_option( 'wpem_rest_api_app_splash_screen_image' );
+        $wpem_app_branding_settings['color_scheme'] = get_option( 'wpem_app_branding_settings' );
+        $wpem_app_branding_settings['dark_color_scheme'] = get_option( 'wpem_app_branding_dark_settings' );
 
-        return apply_filters('wpem_app_branding_settings', $wpem_app_branding_settings);
+        return apply_filters( 'wpem_app_branding_settings', $wpem_app_branding_settings );
     }
 }
-
-
-
 new WPEM_REST_APP_Branding_Controller();
